e410a8bd8520786739de1569cefa67c054ac9811
IT.Sysdev.Philip <johnphilipomamalin1206@gmail.com>
2024-05-09 11:57:50 +0800

    Initial commit

---
+++ src/main/java/userinfo/userinfo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package userinfo;
+
+/**
+ *
+ * @author ph10010886
+ */
+public class userinfo {
+    
+   private String username;
+
+   public String getUsername() {
+       return username;
+   }
+
+   public void setUsername(String username) {
+       this.username = username;
+   }
+
+    public String retrieveSupplier() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+        
+}
